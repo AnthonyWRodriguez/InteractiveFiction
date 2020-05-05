51b2e049c6330940db9a5d6d1b2f569d73992b79
Anthony <awrodriguezt@gmail.com>
2020-05-05 02:36:30 -0600

    Added a Login Icon

--- frontend/src/Components/Header/Header.tsx
+++ frontend/src/Components/Header/Header.tsx
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom'; 
+import { IoIosLogIn } from 'react-icons/io'
 import 'bootstrap/dist/css/bootstrap.css';
 
 export default class Header extends Component<IHeaderProps, IHeaderState>{
@@ -7,9 +9,12 @@ export default class Header extends Component<IHeaderProps, IHeaderState>{
     }
     render(){
         return(
-            <header className="d-flex bg-dark">
-                <h1 className="col-sm-12 p-2 text-danger">Adventure!</h1>
-
+            <header className="d-flex bg-dark align-items-center">
+                <h1 className="col-sm-11 p-2 text-danger">Adventure!</h1>
+                <Link to="/login" className="col-sm-1 align-items-left">
+                    Login
+                    <IoIosLogIn/>
+                </Link>
             </header>
         )
     }
