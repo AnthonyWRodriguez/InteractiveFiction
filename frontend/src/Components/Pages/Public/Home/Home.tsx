f60f319c3523e8c59545fb3e8b69e0961ddab13c
Anthony <awrodriguezt@gmail.com>
2020-05-07 02:29:17 -0600

    Make the play button in home responsive

--- frontend/src/Components/Pages/Public/Home/Home.tsx
+++ frontend/src/Components/Pages/Public/Home/Home.tsx
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Page from '../../Page';
 import 'bootstrap/dist/css/bootstrap.css';
 import {IAuth} from '../../../Common/Interfaces/Interfaces';
 import {removeLocalStorage } from '../../../Utilities/Utilities';
+import { Redirect } from 'react-router-dom';
 
-export default(props: React.PropsWithChildren<IAuth>)=>{
-    removeLocalStorage("potentialEmail");
-    return(
-        <Page auth={props.auth}>
-            <div className="container">
-                <button className="btn btn-danger col-sm-12">PLAY</button>
-            </div>
-        </Page>
-    )
+export default class Login extends Component<IAuth, IHomeState>{
+    constructor(props: IAuth){
+        super(props);
+        this.state={
+            redirect: false,
+            redirectTo: ''
+        }
+    }
+    onClickBtn = (e: React.MouseEvent<HTMLButtonElement>)=>{
+        if(this.props.auth.email){
+            alert("Welcome back "+this.props.auth.name);
+            this.setState({
+                redirect: true,
+                redirectTo: '/game'
+            })
+        }else{
+            alert("To play you need a character first");
+            this.setState({
+                redirect: true,
+                redirectTo: '/login'
+            })
+        }
+    }
+    render(){
+        removeLocalStorage("potentialEmail");
+        if(this.state.redirect){
+            const dir:string = (this.state.redirectTo||'/');
+            return (<Redirect to={dir} />);
+        }
+        return(
+            <Page auth={this.props.auth}>
+                <div className="container">
+                    <button 
+                        className="btn btn-danger col-sm-12"
+                        onClick={this.onClickBtn}
+                    >
+                        PLAY
+                    </button>
+                </div>
+            </Page>
+        )
+    
+    }
+}
+interface IHomeState{
+    redirect: boolean;
+    redirectTo: string;
 }
